fe1dbafa356639e4b9849d181164b011d6325613
tyler liu <replicat0r.misc@gmail.com>
2016-03-22 19:43:12 -0400

    added validation with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = []
@@ -14,16 +15,9 @@ app.get('/todos', function(req, res) {
 })
 
 app.get('/todos/:id', function(req, res) {
-    var todoId = parseInt(req.params.id,10)
-    var matchedTodo
-    todos.forEach(function(todo) {
-        console.log(todo.id)
-        // console.log(matchedTodo)
-        if (todoId === todo.id) {
-           matchedTodo = todo
-        }
-    });
-    console.log(`matched ${matchedTodo}`)
+    var todoId = parseInt(req.params.id, 10)
+    var matchedTodo = _.findWhere(todos, { id: todoId })
+
     if (matchedTodo) {
         res.json(matchedTodo)
 
@@ -32,14 +26,20 @@ app.get('/todos/:id', function(req, res) {
     }
 })
 
-app.post('/todos',function(req,res){
-    var body = req.body;
+app.post('/todos', function(req, res) {
+    var body = _.pick(req.body, 'description', 'completed')
+
+    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length == 0) {
+        return res.status(400).send();
+    }
     //add id field
     //push body into array
+
+    body.description = body.description.trim();
     body.id = todoNextId++;
     todos.push(body)
     res.json(body)
-    
+
 })
 
 app.listen(PORT, function() {
